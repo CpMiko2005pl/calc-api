872311837e7697f9ba5eac700502569bff6e53c2
CpMiko2005pl <donmikolaj@gmail.com>
2025-01-30 23:16:58 +0100

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sympy as sp
 
 app = Flask(__name__)
+CORS(app)  # 🔴 Dodajemy obsługę CORS
 
 @app.route("/integrate", methods=["POST"])
 def integrate():
